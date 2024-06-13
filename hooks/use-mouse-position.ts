4d20e43f5b914602c810063eaa66c8429f53f5ea
morphhyy <imbibek05@gmail.com>
2024-06-13 21:13:04 +0545

    Create a custom hook for mouse position

---
+++ hooks/use-mouse-position.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+export function useMousePosition() {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      setPosition({ x: e.clientX, y: e.clientY });
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  return position;
+}
