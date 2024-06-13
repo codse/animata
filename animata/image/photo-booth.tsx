298652e87ed66ad1afb01034eb8cb0c1aed603fc
Laxmi Lamichhane <68807624+llaxmi@users.noreply.github.com>
2024-06-13 11:40:57 +0545

    Create a photo booth template (#7)

---
+++ animata/image/photo-booth.tsx
@@ -0,0 +1,34 @@
+import { cn } from "@/lib/utils";
+import { HtmlHTMLAttributes } from "react";
+
+interface PhotoBoothProps extends HtmlHTMLAttributes<HTMLDivElement> {
+  collections: string[];
+}
+const PhotoBooth = ({ collections, className, ...props }: PhotoBoothProps) => {
+  return (
+    <div
+      className="group flex w-full max-w-3xl justify-center rounded-lg bg-gray-100 p-3"
+      {...props}
+    >
+      <div className="grid min-w-full grid-cols-2 place-content-center place-items-center gap-5 p-3 sm:grid-cols-4">
+        {collections.map((image, index) => (
+          <div
+            key={`alt_${index}`}
+            className="relative h-[110px] w-[110px] transition-transform duration-700 group-hover:skew-y-6"
+          >
+            <img
+              src={image}
+              alt="photo_booth"
+              className={cn(
+                "absolute inset-0 h-full w-full max-w-full rounded-lg object-cover transition-transform duration-500 hover:scale-125",
+                className,
+              )}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PhotoBooth;
