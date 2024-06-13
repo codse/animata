c5308bfafc983fc5a232644cccebd7a35deeccd0
llaxmi <ll4xmi@gmail.com>
2024-06-13 17:53:57 +0545

    Create a scoreboard tracker

---
+++ animata/widget/score-board.tsx
@@ -0,0 +1,25 @@
+import Histogram from "../graphs/bar-chart";
+
+export default function ScoreBoard({
+  items,
+}: {
+  items: {
+    progress: number;
+    label: string;
+    className?: string;
+    containerClassName?: string;
+  }[];
+}) {
+  return (
+    <div className="group flex min-h-48 w-52 flex-col rounded-lg border-[1px] bg-white p-3 shadow-sm shadow-black/15 transition-all duration-700">
+      <p className="text-base font-semibold text-gray-600">
+        Status:<span className="text-green-700"> Good </span>
+      </p>
+      <Histogram height={76} items={items} />
+      <h5 className="mt-4 text-gray-700">Overall Progress</h5>
+      <p>
+        80% <span className="text-sm text-gray-500">(score, progress)</span>
+      </p>
+    </div>
+  );
+}
