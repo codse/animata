0c40d6e26bf9724f09aa14a64e059c2edf37ef55
acharya-sanjaya <amitacharya22780@gmail.com>
2024-06-13 17:42:51 +0545

    Create a delivery status card

---
+++ animata/widget/delivery-card.stories.tsx
@@ -0,0 +1,25 @@
+import { Meta, StoryObj } from "@storybook/react";
+import DeliveryCard from "./delivery-card";
+
+const meta = {
+  title: "Widget/Delivery Card",
+  component: DeliveryCard,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {},
+} satisfies Meta<typeof DeliveryCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    progress: 30,
+    arrivalTime: "16 : 30",
+    status: "In Transit",
+    location: "Newroad",
+    timeAgo: "30min",
+  },
+};
---
+++ animata/widget/delivery-card.tsx
@@ -0,0 +1,79 @@
+import { cn } from "@/lib/utils";
+import { LocateIcon, TruckIcon } from "lucide-react";
+import React from "react";
+
+interface DeliveryCardProps {
+  progress: number;
+  arrivalTime: string;
+  status: "Pending" | "Processing" | "In Transit" | "Delivered" | "Cancelled";
+  location: string;
+  timeAgo: string;
+}
+
+const DeliveryCard = ({
+  progress,
+  arrivalTime,
+  status,
+  location,
+  timeAgo,
+}: DeliveryCardProps) => {
+  return (
+    <div className="relative min-h-40 w-52 overflow-hidden rounded-2xl text-white">
+      <div
+        className={cn(
+          "absolute right-2 z-10 w-5 bg-[#fff3]",
+          progress < 100 ? "h-32" : "h-24",
+        )}
+      >
+        <div className="absolute -bottom-[10px] left-[2px] h-4 w-full rotate-45 bg-[#3C363D]"></div>
+      </div>
+      <div className="flex h-full flex-col justify-between">
+        <div className="relative w-full bg-[#595856] p-4">
+          <div className="absolute right-2 top-0 flex h-full w-5 justify-center">
+            <div className="z-20 w-[2px] bg-[#595856]"></div>
+          </div>
+          <p className="text-md font-mono">
+            {progress >= 100 ? "Delivered" : "Arrives Today"}
+          </p>
+          {progress < 100 && (
+            <p className="font-mono text-xl font-bold">{arrivalTime}</p>
+          )}
+        </div>
+        <div className="border-t-2 border-t-[#222] bg-[#3C363D] p-4">
+          <p className="text-yellow-400">{status}</p>
+
+          <div className="progress-bar relative my-4 flex h-4 items-center rounded-3xl transition-all">
+            <div className="absolute h-[3px] w-[calc(100%-10px)] rounded-3xl bg-gray-500"></div>
+            <div className="absolute size-3 rounded-full bg-yellow-300"></div>
+            <div className="absolute right-[10px] size-4 rounded-full bg-gray-500"></div>
+            <div className="relative flex w-full items-center p-1">
+              <div
+                className="h-[3px] bg-yellow-300"
+                style={{
+                  width: `${progress < 0 ? 0 : progress > 100 ? 100 : progress}%`,
+                }}
+              ></div>
+              <div
+                className={cn(
+                  "flex size-8 -translate-x-1 items-center justify-center rounded-full p-2",
+                  progress >= 100 ? "bg-green-500" : "bg-yellow-300",
+                )}
+              >
+                <TruckIcon color="black" size={18} />
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-2 flex items-center gap-2 text-gray-400">
+            <LocateIcon />
+            <p>
+              {location} {timeAgo} ago
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DeliveryCard;
