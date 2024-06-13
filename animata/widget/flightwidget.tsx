fc1dd283ae883f7b81e3e77acd45d5c3cf4e7f0b
Aashish Katila <aashishkatila@gmail.com>
2024-06-13 16:12:39 +0545

    Widget for flight detail

---
+++ animata/widget/flightwidget.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { Armchair } from "lucide-react";
+import { useEffect, useState } from "react";
+
+export default function FlightWidget() {
+  const [formattedTime, setFormattedTime] = useState("");
+
+  useEffect(() => {
+    const updateTime = () => {
+      const now = new Date();
+
+      const options: Intl.DateTimeFormatOptions = {
+        day: "2-digit",
+        month: "short",
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false,
+      };
+      const formatted = now.toLocaleString("en-GB", options).replace(",", "");
+
+      setFormattedTime(formatted);
+
+      const secondsUntilNextMinute = 60 - now.getSeconds();
+
+      setTimeout(updateTime, secondsUntilNextMinute);
+    };
+
+    updateTime();
+  }, []);
+
+  return (
+    <div className="flex h-48 w-48 text-black">
+      <div className="z-10 flex h-full w-[25%] flex-col rounded-l-3xl bg-gradient-to-b from-blue-100 to-blue-300">
+        <img
+          src="https://seeklogo.com/images/A/air-canada-logo-A0180CCDB8-seeklogo.com.png"
+          className="mx-auto my-4 h-6 w-6"
+        />
+        <div className="mt-20 w-full -rotate-90 transform text-lg font-semibold tracking-widest text-red-700">
+          AIR&nbsp;CANADA
+        </div>
+      </div>
+      <div className="relative h-full w-[75%] rounded-3xl rounded-l bg-gradient-to-b from-blue-100 to-teal-100 p-2 text-sm">
+        <div className="absolute -left-1 -top-2 z-10 h-4 w-4 rounded-full bg-white"></div>
+        <div className="flex justify-around pb-2">
+          <div className="flex flex-col text-2xl font-medium">
+            <p>TOR</p>
+            <p>NYC</p>
+          </div>
+          <div>
+            <div className="mt-2 rounded-2xl bg-yellow-400 px-2 font-bold">
+              A50
+            </div>
+          </div>
+        </div>
+        <div className="mt-2 px-2 font-bold text-teal-500">FLIGHT</div>
+        <div className="flex items-center justify-around font-bold">
+          <p>AC951</p>
+          <p className="flex pr-2">
+            <Armchair fill="black" /> 1A
+          </p>
+        </div>
+        <div className="mt-2 px-2 font-bold text-teal-500">Date & time</div>
+        <div className="flex pl-2 font-bold">
+          <p>{formattedTime}</p>
+        </div>
+        <div className="absolute -bottom-2 -left-1 z-10 h-4 w-4 rounded-full bg-white"></div>
+      </div>
+    </div>
+  );
+}
