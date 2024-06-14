1059925002eef3bc81cfd96005d61d6a2884d8f8
llaxmi <ll4xmi@gmail.com>
2024-06-14 13:02:41 +0545

    Resolve the commented issue

--- animata/widget/reminder.tsx
+++ animata/widget/reminder.tsx
@@ -5,28 +5,30 @@ export default function Reminder({ className }: { className?: string }) {
   return (
     <div
       className={cn(
-        "relative flex min-h-60 w-52 flex-col justify-between rounded-3xl border-[1px] bg-white p-3 shadow-md duration-700",
+        "relative flex min-h-56 w-full flex-col justify-between rounded-3xl border-[1px] bg-white p-3 shadow-md duration-700",
         className,
       )}
     >
-      <p className="text-center text-xl font-semibold">Reminder</p>
-      <div className="bg-red-7 flex min-h-32 w-full flex-row items-center justify-center gap-x-3">
-        <div className="flex min-h-24 w-1/2 flex-col justify-center rounded-lg bg-purple-200 px-2 text-center text-3xl font-semibold">
+      <p className="text-center text-lg font-semibold">Reminder</p>
+
+      <div className="flex items-center justify-between gap-x-3 p-2">
+        <div className="flex flex-1 flex-col items-center justify-center rounded-lg bg-purple-200 p-4 text-3xl font-semibold">
           2
-          <p className="border-t-[1px] border-white pt-2 text-base font-normal">
+          <p className="border-t-[1px] border-white pt-2 text-center text-base font-normal">
             Work
           </p>
         </div>
-        <div className="flex min-h-24 w-1/2 flex-col justify-center rounded-lg bg-green-200 text-center text-3xl font-semibold">
+        <div className="flex flex-1 flex-col items-center justify-center rounded-lg bg-green-200 p-4 text-3xl font-semibold">
           3
-          <p className="border-t-[1px] border-white pt-2 text-base font-normal">
-            Personal
+          <p className="border-t-[1px] border-white pt-2 text-center text-base font-normal">
+            Home
           </p>
         </div>
       </div>
-      <div className="flex flex-row gap-2">
+
+      <div className="flex flex-row gap-1">
         <ArrowRight size={18} className="self-center" />
-        Meeting in 30 mins
+        <p className="text-md font-semibold">Meeting in 30 mins</p>
       </div>
     </div>
   );
