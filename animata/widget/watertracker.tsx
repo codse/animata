bbece42914deeaa638af86eea75dd2b5c75d13ac
GrgSumin <be2019se673@gces.edu.np>
2024-06-17 13:26:10 +0545

    Move progress bar from bottom to right side.

--- animata/widget/watertracker.tsx
+++ animata/widget/watertracker.tsx
@@ -4,7 +4,7 @@ import { Droplet } from "lucide-react";
 import { cn } from "@/lib/utils";
 
 export default function Watertracker() {
-  const [waterIntake, setWaterIntake] = useState(500);
+  const [waterIntake, setWaterIntake] = useState(1500);
   const dailyGoal = 3000;
   const [initialLoad, setInitialLoad] = useState(true);
 
@@ -24,7 +24,9 @@ export default function Watertracker() {
   const progress = (waterIntake / dailyGoal) * 100;
 
   return (
-    <div className={cn("group relative h-40 w-52 rounded-3xl bg-gray-800 p-3")}>
+    <div
+      className={cn("group relative flex h-40 w-44 rounded-3xl bg-gray-800")}
+    >
       <style jsx>{`
         .progress-bar {
           animation: load 0.5s forwards;
@@ -32,44 +34,49 @@ export default function Watertracker() {
 
         @keyframes fill {
           from {
-            width: 0;
+            height: 0;
           }
           to {
-            width: var(--progress-width);
+            height: var(--progress-height);
           }
         }
 
         .progress-bar-fill {
-          animation: fill 0.3s forwards;
+          animation: fill 2s forwards;
         }
       `}</style>
-      <div className="flex items-center justify-center gap-1">
-        <Droplet color="#1d4ed8" size={22} />
-        <p className="text-sm font-semibold text-white">Water</p>
+      <div className="flex w-full flex-col pr-9 pt-2">
+        <div className="mb-auto flex items-center justify-center gap-1">
+          <Droplet color="#1d4ed8" size={22} />
+          <p className="text-sm font-semibold text-white">Water</p>
+        </div>
+        <div className="mb-auto flex justify-center">
+          <h1 className="mt-1 text-xl font-bold text-white">
+            {waterIntake} ml
+          </h1>
+        </div>
+        <div className="mb-auto flex justify-center">
+          <p className="font-sans text-xs text-gray-400">Today</p>
+        </div>
+        <div className="mb-2 flex justify-center">
+          <button
+            onClick={handleAddWater}
+            className="h-8 w-24 transform rounded-full bg-gradient-to-r from-blue-500 to-blue-700 text-sm font-semibold text-white transition-transform hover:scale-105 hover:shadow-lg"
+          >
+            +250 ml
+          </button>
+        </div>
       </div>
-      <div className="flex justify-center">
-        <h1 className="mt-1 text-xl font-bold text-white">{waterIntake} ml</h1>
-      </div>
-      <div className="flex justify-center">
-        <p className="font-sans text-xs text-gray-400">Today</p>
-      </div>
-      <div className="flex justify-center">
-        <button
-          onClick={handleAddWater}
-          className="mt-2 h-8 w-24 transform rounded-full bg-gradient-to-r from-blue-500 to-blue-700 text-sm font-semibold text-white transition-transform hover:scale-105 hover:shadow-lg"
-        >
-          +250 ml
-        </button>
-      </div>
-      <div className="absolute bottom-0 left-0 w-full">
-        <div className="relative h-8 w-full overflow-hidden rounded-b-3xl border-t-2 bg-gray-800">
+
+      <div className="relative flex flex-col items-end">
+        <div className="absolute bottom-0 right-0 h-full w-10 overflow-hidden rounded-r-3xl border-l-2 border-gray-700 bg-gray-800">
           <div
-            className={`absolute left-0 top-0 h-8 bg-gradient-to-r from-blue-500 to-blue-700 transition-all duration-300 ease-in-out ${
+            className={`absolute bottom-0 right-0 w-full bg-gradient-to-t from-blue-500 to-blue-700 transition-all duration-300 ease-in ${
               initialLoad ? "progress-bar" : "progress-bar-fill"
             }`}
             style={{
-              width: `${progress}%`,
-              "--progress-width": `${progress}%`,
+              height: `${progress}%`,
+              "--progress-height": `${progress}%`,
             }}
           ></div>
         </div>
