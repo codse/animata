4a4e2418c20f4463382b1dec3957ee676161409d
GrgSumin <be2019se673@gces.edu.np>
2024-06-18 11:33:13 +0545

    Add twitter id and remove scrollbar

--- animata/widget/reminder-widget.tsx
+++ animata/widget/reminder-widget.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { cn } from "@/lib/utils";
 import { BellDot } from "lucide-react";
+import { useState } from "react";
 
 export default function ReminderWidget() {
   const reminders = [
@@ -11,19 +12,35 @@ export default function ReminderWidget() {
     "key",
     "mobile",
   ];
+  const [reminder, setReminder] = useState<string[]>(reminders);
+
+  const handleCheckboxChange = (data: string) => {
+    setReminder((prev) =>
+      prev.includes(data)
+        ? prev.filter((remind) => remind !== data)
+        : [...prev, data],
+    );
+  };
   return (
     <div
       className={cn("group min-h-40 w-52 rounded-3xl bg-zinc-900 px-4 py-3")}
     >
-      <div className="text-md flex items-center justify-start gap-2 font-bold text-blue-400">
-        <p>Reminder</p>
-        <BellDot color="#60a5fa" size={15} className="mt-px" />
+      <div className="text-md flex items-center justify-between gap-2 font-bold text-blue-400">
+        <div className="flex items-center gap-2">
+          <p>Reminder</p>
+          <BellDot color="#60a5fa" size={15} className="mt-1" />
+        </div>
+        <div className="">
+          <p>{reminder.length}</p>
+        </div>
       </div>
-      <div className="custom-scrollbar mt-1 h-32 overflow-y-auto">
+      <div className="mt-1 overflow-hidden">
         {reminders.map((data) => (
           <div className="flex items-center gap-3 border-b-[1px] border-gray-700">
             <input
               type="checkbox"
+              onChange={() => handleCheckboxChange(data)}
+              checked={!reminder.includes(data)}
               className="h-3 w-3 appearance-none rounded-full border-2 border-gray-700 checked:bg-blue-500"
             />
             <p className="mt-1 bg-transparent pb-2 text-xs font-semibold text-white">
@@ -32,22 +49,6 @@ export default function ReminderWidget() {
           </div>
         ))}
       </div>
-
-      <style jsx>{`
-        .custom-scrollbar::-webkit-scrollbar {
-          width: 4px;
-        }
-        .custom-scrollbar::-webkit-scrollbar-track {
-          background: transparent;
-        }
-        .custom-scrollbar::-webkit-scrollbar-thumb {
-          background-color: #6b7280;
-          border-radius: 8px;
-        }
-        .custom-scrollbar::-webkit-scrollbar-thumb:hover {
-          background-color: #4b5563;
-        }
-      `}</style>
     </div>
   );
 }
