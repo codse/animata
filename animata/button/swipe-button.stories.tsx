4752eb059d8cb4b250c3f0b1fe74ce46b028d320
GrgSumin <be2019se673@gces.edu.np>
2024-06-20 16:36:04 +0545

    Done changes according to review

--- animata/button/swipe-button.stories.tsx
+++ animata/button/swipe-button.stories.tsx
@@ -17,11 +17,9 @@ type Story = StoryObj<typeof meta>;
 export const Primary: Story = {
   args: {
     borderColor: "#192de3",
-    firstBg: "#f5cb42",
-    secondBg: "#080807",
-    firstTextColor: "#192de3",
-    secondTextColor: "#f5f6fc",
-    secondtext: "Get Passes",
-    firsttext: "Get Passes",
+    secondtext: "Get Passesssssssssss",
+    firsttext: "Get Passessssssssssss",
+    firstClass: "bg-red-500 text-blue-800",
+    secondClass: "bg-black text-white",
   },
 };
--- animata/button/swipe-button.tsx
+++ animata/button/swipe-button.tsx
@@ -1,16 +1,14 @@
 "use client";
 import { cn } from "@/lib/utils";
-import React from "react";
+import React, { useState } from "react";
 interface SwipeButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   firsttext: string;
   secondtext: string;
   borderColor?: string;
   className?: string;
-  firstBg: string;
-  secondBg: string;
-  firstTextColor: string;
-  secondTextColor: string;
+  firstClass?: string;
+  secondClass?: string;
 }
 
 export default function SwipeButton({
@@ -18,10 +16,8 @@ export default function SwipeButton({
   secondtext,
   className,
   borderColor,
-  firstBg,
-  secondBg,
-  firstTextColor,
-  secondTextColor,
+  firstClass,
+  secondClass,
   ...props
 }: SwipeButtonProps) {
   return (
@@ -29,22 +25,22 @@ export default function SwipeButton({
       style={{ borderColor: borderColor }}
       {...props}
       className={cn(
-        "group relative inline-flex min-h-16 w-48 items-center justify-center overflow-hidden rounded-md border-2 p-3 font-medium shadow-md transition duration-500 ease-out",
+        "group relative overflow-hidden rounded-md border-2 font-medium transition duration-500 ease-out",
         className,
       )}
     >
       <span
-        style={{ backgroundColor: secondBg, color: secondTextColor }}
         className={cn(
-          "ease absolute inset-0 flex h-full w-full translate-y-full items-center justify-center text-2xl font-bold duration-500 group-hover:translate-y-0",
+          "ease absolute flex h-full w-full translate-y-full items-center justify-center px-2 py-3 text-2xl font-bold duration-500 group-hover:translate-y-0",
+          secondClass,
         )}
       >
         {secondtext}
       </span>
       <span
-        style={{ backgroundColor: firstBg, color: firstTextColor }}
         className={cn(
-          "ease absolute flex h-full w-full transform items-center justify-center text-2xl font-bold transition-all duration-500 group-hover:-translate-y-full",
+          "ease flex items-center justify-center px-2 py-3 text-2xl font-bold duration-500 group-hover:-translate-y-full",
+          firstClass,
         )}
       >
         {firsttext}
