03a55b2897b0781463970f49ac69a4af8f7a020c
Hari Lamichhane <onwithhari@gmail.com>
2024-06-20 07:26:40 +0545

    Fix build

--- components/component-preview.tsx
+++ components/component-preview.tsx
@@ -92,7 +92,7 @@ export function ComponentPreview({
           }
         >
           <iframe
-            src={`${process.env.NEXT_PUBLIC_STORYBOOK_URL}/iframe.html?globals=backgrounds.grid:!false;theme:${theme ?? localStorage.getItem("theme")};backgrounds.value:!transparent&viewMode=docs&id=${name}&site:docs=true`}
+            src={`${process.env.NEXT_PUBLIC_STORYBOOK_URL}/iframe.html?globals=backgrounds.grid:!false;theme:${theme ?? (typeof localStorage !== "undefined" ? localStorage?.getItem?.("theme") : "")};backgrounds.value:!transparent&viewMode=docs&id=${name}&site:docs=true`}
             className="w-full"
             style={{
               height: `${Math.max(200, minHeight)}px`,
