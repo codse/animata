2f4a4e25b197a88d6232769e56f477f90497f729
GrgSumin <be2019se673@gces.edu.np>
2024-06-21 13:01:14 +0545

    Create a menu animation

---
+++ animata/list/menu-animation.stories.tsx
@@ -0,0 +1,19 @@
+import MenuAnimation from "@/animata/list/menu-animation";
+import { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+  title: "List/Menu Animation",
+  component: MenuAnimation,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {},
+} satisfies Meta<typeof MenuAnimation>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {},
+};
---
+++ animata/list/menu-animation.tsx
@@ -0,0 +1,30 @@
+import { ArrowRight } from "lucide-react";
+
+export default function MenuAnimation() {
+  const menuItems = [
+    "Home",
+    "Features",
+    "Company",
+    "FAQs",
+    "Terms of Use",
+    "Get In Touch",
+  ];
+
+  return (
+    <div className="flex flex-col overflow-hidden p-4">
+      {menuItems.map((item, index) => (
+        <div key={index} className="group flex items-center">
+          <div className="relative">
+            <ArrowRight
+              className="absolute transform text-black opacity-0 transition-transform duration-300 ease-out hover:z-20 group-hover:translate-x-0 group-hover:text-blue-500 group-hover:opacity-100"
+              size={20}
+            />
+            <h1 className="z-10 cursor-pointer font-mono font-semibold text-black transition-transform duration-300 ease-out group-hover:translate-x-[30px] group-hover:text-blue-500 dark:text-white">
+              {item}
+            </h1>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
