4e3bdf6dd3546a24ab9306b13d9626208f298196
Bibek <61041540+morphhyy@users.noreply.github.com>
2024-06-23 08:29:38 +0545

    Create a text masking (#27)

---
+++ animata/text/mask-text.stories.tsx
@@ -0,0 +1,29 @@
+import MaskText from "@/animata/text/mask-text";
+import { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+  title: "Text/Mask Text",
+  component: MaskText,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {},
+} satisfies Meta<typeof MaskText>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    className: "h-[30rem]",
+    revealText:
+      "I'm a selectively skilled full-stack developer with keen enthusiasm for mobile/web application.",
+    originalText: (
+      <p>
+        I'm a <span className="text-rose-900">selectively skilled</span> product
+        designer with strong focus.
+      </p>
+    ),
+  },
+};
