7d312a9f84c3173da741170916685a04268f3efc
AadarshBaral <77476239+AadarshBaral@users.noreply.github.com>
2024-06-23 08:29:57 +0545

    Create a direction card component (#46)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -59,6 +59,7 @@ const config = {
       },
       boxShadow: {
         glow: "0 0 20px rgba(255, 204, 112, 0.7), 0 0 40px rgba(200, 80, 192, 0.5), 0 0 60px rgba(65, 88, 208, 0.3)",
+        glow2 : "0 0 20px rgba(50, 255, 50, 0.7), 0 0 40px rgba(20, 200, 20, 0.5), 0 0 60px rgba(5, 150, 5, 0.3)"
       },
       filter: {
         "blur-20": "blur(20px)",
