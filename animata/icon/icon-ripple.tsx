aaa42a3e796f1333e078994f35c2aa7f0f0dfd1c
Hari Lamichhane <onwithhari@gmail.com>
2024-06-25 14:04:22 +0545

    fix: storybook build issue (#95)

--- animata/icon/icon-ripple.tsx
+++ animata/icon/icon-ripple.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { cn } from "@/lib/utils";
 import { Mic } from "lucide-react";
 
+import { cn } from "@/lib/utils";
 
 interface IconRippleProps extends React.HTMLAttributes<HTMLDivElement> {
   /**
@@ -14,13 +14,13 @@ interface IconRippleProps extends React.HTMLAttributes<HTMLDivElement> {
    */
   iconSize?: number;
   /**
-   * Colour of the Icon
+   * Color of the Icon
    */
   iconColor?: string;
   /**
-   * Border colour that will have ripple animation
+   * Border color that will have ripple animation
    */
-  borderColour?: string;
+  borderColor?: string;
   /**
    * Padding around the icon
    */
@@ -31,11 +31,11 @@ export default function IconRipple({
   icon: Icon = Mic,
   iconSize = 24,
   iconColor = "#ddd",
-  borderColour = "#ddd",
+  borderColor = "#ddd",
   inset = "10px",
 }: IconRippleProps) {
   const customBorderStyle = {
-    borderColor: borderColour,
+    borderColor,
   };
   const insetStyle = {
     top: `-${inset}`,
@@ -46,11 +46,6 @@ export default function IconRipple({
 
   return (
     <div className={cn("group relative flex items-center justify-center")}>
-      <Icon size={iconSize} color={iconColor} />
-      <div
-        className="absolute -inset-4 animate-ping rounded-full border-2"
-  return (
-    <div className={cn("relative flex items-center justify-center")}>
       <Icon size={iconSize} color={iconColor} />
       <div
         className={cn("absolute -inset-4 rounded-full border-2 hover:animate-ping")}
