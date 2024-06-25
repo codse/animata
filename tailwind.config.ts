b6d0a795e2d0da81c5ee684940e731042751636d
AashishKatila <61934354+AashishKatila@users.noreply.github.com>
2024-06-25 12:49:32 +0545

    fix: update glowing card (#40)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -58,6 +58,14 @@ const config = {
         sm: "calc(var(--radius) - 4px)",
       },
       boxShadow: {
+        'glow': '0 0 20px rgba(255, 204, 112, 0.7), 0 0 40px rgba(200, 80, 192, 0.5), 0 0 60px rgba(65, 88, 208, 0.3)',
+      },
+      filter: {
+        'blur-20': 'blur(20px)',
+        'blur-25': 'blur(25px)',
+      },
+      brightness: {
+        150: '1.5',
         glow: "0 0 20px rgba(255, 204, 112, 0.7), 0 0 40px rgba(200, 80, 192, 0.5), 0 0 60px rgba(65, 88, 208, 0.3)",
         glow2:
           "0 0 20px rgba(50, 255, 50, 0.7), 0 0 40px rgba(20, 200, 20, 0.5), 0 0 60px rgba(5, 150, 5, 0.3)",
