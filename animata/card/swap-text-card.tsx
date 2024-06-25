a4a331e0c0137389b2b404d77e2d6370425aaeec
Hari Lamichhane <onwithhari@gmail.com>
2024-06-25 17:42:40 +0545

    fix: improve examples

--- animata/card/swap-text-card.tsx
+++ animata/card/swap-text-card.tsx
@@ -7,22 +7,23 @@ interface FlipTextCardProps {
 
 export default function SwapTextCard({ initialText, finalText }: FlipTextCardProps) {
   return (
-    <div className="flex h-64 w-[480px] flex-col justify-between rounded-md border-2 bg-gray-200 p-4 md:w-[500px]">
-      <h5 className="text-gray-500">Animata</h5>
-      <div className="flex h-20 flex-col justify-between md:w-72">
+    <div className="group flex min-h-64 w-full flex-col justify-between rounded-3xl bg-gray-100 p-6 md:max-w-[500px]">
+      <h5 className="mb-2 text-sm font-medium uppercase tracking-wide text-gray-500">Animata</h5>
+      <div className="flex flex-col justify-between md:min-w-72">
         <div className="md:hidden">
           <div className="text-lg font-semibold text-black">{initialText}</div>
           <div className="text-sm font-medium text-gray-500">{finalText}</div>
         </div>
-        <div className="hidden h-full md:flex">
-          <SwapText
-            initialText={initialText}
-            finalText={finalText}
-            className="h-full"
-            initialTextClassName="text-lg font-semibold text-black w-full"
-            finalTextClassName="text-sm font-medium text-gray-500 h-full w-full"
-          />
-        </div>
+        <SwapText
+          initialText={initialText}
+          finalText={finalText}
+          disableClick
+          // Set min height so that all the text content fits
+          // use -mb-7 to hide the extra space when not active
+          className="-mb-7 hidden min-h-20 w-3/4 transition-all duration-200 group-hover:mb-0 md:flex md:flex-col"
+          initialTextClassName="text-lg group-hover:opacity-0 h-full duration-200 font-semibold text-black"
+          finalTextClassName="text-sm h-full duration-200 font-medium text-gray-500"
+        />
       </div>
     </div>
   );
