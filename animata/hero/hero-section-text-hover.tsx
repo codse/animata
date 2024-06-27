5aa8ba775df529c01ad55aa989c76143e222e96a
chiranjibi <ranachiranjibi112233@gmail.com>
2024-06-27 11:42:58 +0545

    fix: change image to emoji and make responsive

--- animata/hero/hero-section-text-hover.tsx
+++ animata/hero/hero-section-text-hover.tsx
@@ -1,11 +1,8 @@
 import React from "react";
-import Image, { StaticImageData } from "next/image";
 import { Map } from "lucide-react";
 
-import img1 from "@/public/bg-hills.jpg";
-
 interface DestinationProps {
-  img: StaticImageData;
+  emoji: string;
   position: string;
 }
 
@@ -18,67 +15,73 @@ interface HeroCardProps {
   destinations?: DestinationProps[];
   treasures?: TreasureProps[];
 }
+
 const HeroSectionTextHover: React.FC<HeroCardProps> = () => {
   const destinations: DestinationProps[] = [
     {
-      img: img1, //first image
-      position: "-left-20 top-0 group-hover:-rotate-[10deg]  group-hover:-translate-y-16",
-    },
-    {
-      img: img1, //second image
-      position: " -left-24 top-0 group-hover:-rotate-[20deg]  group-hover:-translate-x-12",
-    },
-    {
-      img: img1, //third image
+      emoji: "🪂",
       position:
-        " left-[240px]  top-0 group-hover:rotate-[10deg] -top-3  group-hover:-translate-y-16",
+        "-left-20 top-3 group-hover:-rotate-[10deg] group-hover:-translate-y-12 md:-left-28 md:-top-2 sm:-left-24",
     },
     {
-      img: img1, //fourth image
-      position: "left-[220px] -top-1 group-hover:rotate-[20deg] group-hover:translate-x-16",
+      emoji: "🏖️",
+      position:
+        "-left-[72px] top-0 group-hover:-rotate-[20deg] group-hover:-translate-x-10 md:-left-[135px] md:-top-2 sm:-left-24 ",
+    },
+    {
+      emoji: "🚁",
+      position:
+        "left-[150px] top-0 group-hover:rotate-[10deg] group-hover:-translate-y-10 md:left-[210px] md:-top-1 sm:left-[180px]",
+    },
+    {
+      emoji: "🏯",
+      position:
+        "left-[105px] top-0 group-hover:rotate-[20deg] group-hover:translate-x-16 md:left-[190px] md:-top-2 sm:left-[150px]",
     },
   ];
 
   const treasures: TreasureProps[] = [
     {
       emoji: "🦝",
-      position: " -left-32 -top-16 -rotate-[30deg]  group-hover:-translate-y-8 ",
+      position:
+        "-left-[100px] -top-7 -rotate-[30deg] group-hover:-translate-y-8 md:-left-40 md:-top-16 sm:-left-32",
     },
     {
       emoji: "🍜",
-      position: " -left-40 -top-2 group-hover:-rotate-45",
+      position: "-left-[115px] -top-2 group-hover:-rotate-45 md:-left-44 md:-top-1 sm:-left-36",
     },
     {
       emoji: "🏝️",
-      position: " left-[200px] -top-20 rotate-[30deg] ",
+      position:
+        "left-32 -top-12 rotate-[30deg] md:left-[200px] md:-top-[70px] sm:left-[175px] sm:-top-12",
     },
     {
       emoji: "💎",
-      position: " left-[200px] -top-1  group-hover:rotate-[45deg]",
+      position:
+        "left-32 -top-2 group-hover:rotate-[45deg] md:left-[200px] md:-top-1 sm:left-[160px] ",
     },
   ];
 
   return (
-    <div className="relative min-h-[100px] w-screen rounded-2xl border-2 border-gray-300">
+    <div className="relative min-h-[100px] w-full rounded-2xl border-2 border-gray-300 md:min-h-[200px]">
       <div className="mb-2 flex cursor-pointer flex-col items-center justify-center gap-3">
-        <div className="flex flex-col items-center justify-center p-5 text-2xl font-bold">
+        <div className="text-normal flex flex-col items-center justify-center p-5 font-bold sm:text-xl md:text-2xl">
           <div className="mt-5">
             <Map size={40} className="fill-zinc-900 text-white" />
           </div>
-
           <div className="flex items-center justify-center gap-1">
-            <span className="text-gray-400">Explore</span>
+            <span className="text-gray-400">Embark on</span>
             <div className="group relative flex items-center">
-              <span className="text-zinc-500 group-hover:text-sky-400">Uncharted Destinations</span>
-
-              <div className="absolute inset-0 opacity-0 transition-opacity [transition-duration:400ms] group-hover:opacity-100">
+              <span className="text-zinc-500 group-hover:text-sky-400">Uncharted Adventures</span>
+              <div className="duration-400 absolute inset-0 opacity-0 transition-opacity group-hover:opacity-100">
                 {destinations.map((dest, index) => (
-                  <Image
+                  <span
                     key={index}
-                    src={dest.img}
-                    alt={`destination ${index}`}
-                    className={`absolute h-[40px] w-[40px] transform rounded-md transition-transform duration-500 group-hover:scale-110 ${dest.position}`}
-                  />
+                    className={`absolute transform text-lg transition-transform duration-500 group-hover:scale-110 sm:text-2xl md:text-4xl ${dest.position}`}
+                    role="img"
+                  >
+                    {dest.emoji}
+                  </span>
                 ))}
               </div>
             </div>
@@ -87,13 +90,12 @@ const HeroSectionTextHover: React.FC<HeroCardProps> = () => {
           <div className="flex items-center justify-center gap-1">
             <span className="text-gray-400">and</span>
             <div className="group relative flex items-center">
-              <span className="text-zinc-500 group-hover:text-orange-500">Local Gems</span>
-
-              <div className="absolute inset-0 opacity-0 transition-opacity [transition-duration:400ms] group-hover:opacity-100">
+              <span className="text-zinc-500 group-hover:text-orange-500">Feel Alive</span>
+              <div className="duration-400 absolute inset-0 opacity-0 transition-opacity group-hover:opacity-100">
                 {treasures.map((gem, index) => (
                   <span
                     key={index}
-                    className={`absolute transform text-4xl transition-transform duration-500 group-hover:scale-110 ${gem.position} `}
+                    className={`absolute transform text-lg transition-transform duration-500 group-hover:scale-110 sm:text-2xl md:text-4xl ${gem.position}`}
                     role="img"
                   >
                     {gem.emoji}
@@ -104,7 +106,7 @@ const HeroSectionTextHover: React.FC<HeroCardProps> = () => {
           </div>
         </div>
         <button className="rounded-3xl bg-orange-400 px-4 py-2 font-mono tracking-tighter hover:bg-orange-500">
-          start your journey
+          Begin your journey
         </button>
       </div>
     </div>
