1974011499dcbdf801995e2141dd05f89dc05610
Sudha Shrestha <itsmesudha12@gmail.com>
2024-06-28 09:21:34 +0545

    feat: add avatar list component

--- animata/list/avatar-list.tsx
+++ animata/list/avatar-list.tsx
@@ -1,7 +1,65 @@
+const data = [
+  {
+    name: "John Doe",
+    position: "Software Engineer",
+    image:
+      "https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Jane Smith",
+    position: "Product Manager",
+    image:
+      "https://images.unsplash.com/photo-1582610285985-a42d9193f2fd?q=80&w=800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "David Johnson",
+    position: "UX Designer",
+    image:
+      "https://images.unsplash.com/photo-1530268729831-4b0b9e170218?q=80&w=800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Emily Davis",
+    position: "Frontend Developer",
+    image:
+      "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Michael Wilson",
+    position: "Backend Developer",
+    image:
+      "https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?q=80&w=800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Sarah Thompson",
+    position: "Data Scientist",
+    image:
+      "https://images.unsplash.com/photo-1573496799515-eebbb63814f2?q=80&w=800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
+
 export default function AvatarList() {
   return (
-    <div>
-      <p>Let your <i>imagination</i> run wild with this template</p>
+    <div className="flex py-12">
+      {data.map((item) => (
+        <div
+          key={item.name}
+          className="scale-1 group relative z-0 -ml-4 flex items-center transition-all duration-300 ease-in-out hover:z-10 hover:scale-110"
+        >
+          <div className="relative overflow-hidden rounded-full bg-white">
+            <div className="bg-size pointer-events-none absolute h-full w-full animate-bg-position from-violet-500 from-30% via-cyan-400 via-50% to-pink-500 to-80% bg-[length:300%_auto] opacity-15 group-hover:bg-gradient-to-r" />
+            <div className="z-1 blur-lg" />
+            <img
+              src={item.image}
+              alt={item.name}
+              className="m-2 size-12 rounded-full object-cover"
+            />
+          </div>
+          <div className="absolute bottom-full left-1/2 -translate-x-1/2 translate-y-2 transform whitespace-nowrap rounded bg-slate-900 p-2 text-white opacity-0 transition-all duration-300 ease-in-out group-hover:-translate-y-2 group-hover:opacity-100 dark:bg-slate-100 dark:text-slate-900">
+            <div className="text-sm font-semibold">{item.name}</div>
+            <div className="text-sm">{item.position}</div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
