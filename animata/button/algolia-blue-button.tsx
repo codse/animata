2e8ab97ba866205817e820c6e14d1f397e1516d2
Hari Lamichhane <onwithhari@gmail.com>
2024-07-06 00:17:36 +0545

    feat: add new buttons (#141)

---
+++ animata/button/algolia-blue-button.tsx
@@ -0,0 +1,10 @@
+export default function AlgoliaBlueButton() {
+  return (
+    <button
+      className="relative box-border inline-flex h-12 cursor-pointer touch-manipulation items-center justify-center overflow-hidden whitespace-nowrap rounded-md border-0 bg-gradient-to-r from-sky-500 to-blue-600 px-4 font-mono leading-none text-white no-underline shadow-[rgba(45,35,66,0.4)_0_2px_4px,rgba(45,35,66,0.3)_0_7px_13px_-3px,rgba(58,65,111,0.5)_0_-3px_0_inset] transition-all duration-150 ease-in-out hover:-translate-y-0.5 hover:shadow-[rgba(45,35,66,0.4)_0_4px_8px,rgba(45,35,66,0.3)_0_7px_13px_-3px,#3c4fe0_0_-3px_0_inset] focus:shadow-[#3c4fe0_0_0_0_1.5px_inset,rgba(45,35,66,0.4)_0_2px_4px,rgba(45,35,66,0.3)_0_7px_13px_-3px,#3c4fe0_0_-3px_0_inset] active:translate-y-0.5 active:shadow-[#3c4fe0_0_3px_7px_inset]"
+      role="button"
+    >
+      Aloglia Blue
+    </button>
+  );
+}
