6086be9cc8743213712d73e30dd21023ebdb6b2b
Hari Lamichhane <onwithhari@gmail.com>
2024-07-16 13:33:57 +0545

    fix: flickering issue (#155)

--- animata/text/double-underline.tsx
+++ animata/text/double-underline.tsx
@@ -6,20 +6,17 @@ export default function DoubleUnderline({
   ...props
 }: React.HTMLProps<HTMLSpanElement>) {
   const common =
-    "absolute h-[2px] w-full bg-blue-500 transition-all duration-200 group-hover:opacity-50 dark:bg-white/70";
+    "absolute h-px w-full bg-blue-500 transition-all duration-200 group-hover:opacity-50 dark:bg-white/70";
   return (
     <span
       {...props}
-      className={cn(
-        "group relative inline-block cursor-pointer text-blue-500",
-        className,
-      )}
+      className={cn("group relative inline-block cursor-pointer text-blue-500", className)}
     >
       {children}
       <span
         className={cn(
           common,
-          "left-0 top-[calc(100%_+_4px)] group-hover:top-0",
+          "pointer-events-none left-0 top-[calc(100%_-_2px)] group-hover:top-0",
         )}
       />
       <span className={cn(common, "-bottom-[2px] left-0")} />
