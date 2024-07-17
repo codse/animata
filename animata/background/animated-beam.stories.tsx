9517c771b2bf8e60a1d98be15d45a656e4033672
Hari Lamichhane <onwithhari@gmail.com>
2024-07-17 17:07:35 +0545

    fix: text color in light mode

--- animata/background/animated-beam.stories.tsx
+++ animata/background/animated-beam.stories.tsx
@@ -17,7 +17,7 @@ type Story = StoryObj<typeof meta>;
 export const Primary: Story = {
   args: {
     children: (
-      <div className="flex min-h-96 w-full flex-col items-center justify-center text-xl text-foreground md:text-3xl">
+      <div className="flex min-h-96 w-full flex-col items-center justify-center text-xl text-white md:text-3xl">
         <div className="max-w-lg rounded-xl bg-opacity-10 p-4 text-center font-light backdrop-blur-sm">
           Look at the <span className="line-through">stars</span> <strong>meteors</strong>! Look how
           they <span className="line-through">shine</span> <strong>fall</strong> for you!
