a05072a4a976196e944dc916c9acd838835fd4c8
Hari Lamichhane <onwithhari@gmail.com>
2024-07-17 16:35:26 +0545

    feat: create animated beam component (#159)

---
+++ animata/background/animated-beam.stories.tsx
@@ -0,0 +1,31 @@
+import AnimatedBeam from "@/animata/background/animated-beam";
+import { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+  title: "Background/Animated Beam",
+  component: AnimatedBeam,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {},
+} satisfies Meta<typeof AnimatedBeam>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    children: (
+      <div className="flex min-h-96 w-full flex-col items-center justify-center text-xl text-foreground md:text-3xl">
+        <div className="max-w-lg rounded-xl bg-opacity-10 p-4 text-center font-light backdrop-blur-sm">
+          Look at the <span className="line-through">stars</span> <strong>meteors</strong>! Look how
+          they <span className="line-through">shine</span> <strong>fall</strong> for you!
+          <small className="mt-5 block text-sm text-opacity-30">
+            This is a placeholder content
+          </small>
+        </div>
+      </div>
+    ),
+  },
+};
--- animata/text/cycle-text.tsx
+++ animata/text/cycle-text.tsx
@@ -1,16 +1,17 @@
-import { AnimatePresence, motion } from "framer-motion";
 import { useEffect, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function CycleText() {
   const words = ["Hello", "World", "Ciaoo", "World"];
   const [index, setIndex] = useState(0);
 
+  const total = words.length;
   useEffect(() => {
     const interval = setInterval(() => {
-      setIndex((index + 1) % words.length);
+      setIndex((current) => (current + 1) % total);
     }, 1300);
     return () => clearInterval(interval);
-  });
+  }, [total]);
 
   return (
     <div>
@@ -25,7 +26,7 @@ export default function CycleText() {
             transition={{ duration: 0.08 }}
             className="inline-block font-mono text-xl text-blue-700"
           >
-            "{words[index]}"
+            &quot;{words[index]}&quot;
           </motion.h1>
         </AnimatePresence>
         )
