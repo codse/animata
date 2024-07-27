ced77b187413cbb93c06342662d9ca1ace1f8e55
Hari Lamichhane <onwithhari@gmail.com>
2024-07-27 14:04:26 +0545

    fix: marquee

--- animata/container/marquee.tsx
+++ animata/container/marquee.tsx
@@ -43,42 +43,41 @@ export default function Marquee({
   ...props
 }: MarqueeProps) {
   return (
-    <>
-      <div
-        {...props}
-        className={cn(
-          "group relative flex h-full w-full overflow-hidden p-2 [--duration:10s] [--gap:12px] [gap:var(--gap)]",
-          {
-            "flex-col": vertical,
-            "flex-row": !vertical,
-          },
-          className,
-        )}
-      >
-        {Array.from({ length: repeat }).map((_, index) => (
-          <div
-            key={`item-${index}`}
-            className={cn("flex shrink-0 [gap:var(--gap)]", {
-              "group-hover:[animation-play-state:paused]": pauseOnHover,
-              "[animation-direction:reverse]": reverse,
-              "animate-marquee-horizontal flex-row": !vertical,
-              "animate-marquee-vertical flex-col": vertical,
-            })}
-          >
-            {children}
-          </div>
-        ))}
-      </div>
+    <div
+      {...props}
+      className={cn(
+        "group relative flex h-full w-full p-2 [--duration:10s] [--gap:12px] [gap:var(--gap)]",
+        {
+          "flex-col": vertical,
+          "flex-row": !vertical,
+        },
+        className,
+      )}
+    >
+      {Array.from({ length: repeat }).map((_, index) => (
+        <div
+          key={`item-${index}`}
+          className={cn("flex shrink-0 [gap:var(--gap)]", {
+            "group-hover:[animation-play-state:paused]": pauseOnHover,
+            "[animation-direction:reverse]": reverse,
+            "animate-marquee-horizontal flex-row": !vertical,
+            "animate-marquee-vertical flex-col": vertical,
+          })}
+        >
+          {children}
+        </div>
+      ))}
       {applyMask && (
         <div
-          className={cn("pointer-events-none absolute z-10 h-full w-full", {
-            "bg-gradient-to-b from-white/50 from-5% via-transparent via-50% to-white/50 to-95% dark:from-black/50 dark:to-black/50":
-              vertical,
-            "bg-gradient-to-r from-white/50 from-5% via-transparent via-50% to-white/50 to-95% dark:from-black/50 dark:to-black/50":
-              !vertical,
-          })}
+          className={cn(
+            "pointer-events-none absolute inset-0 z-10 h-full w-full from-white/50 from-5% via-transparent via-50% to-white/50 to-95% dark:from-gray-800/50 dark:via-transparent dark:to-gray-800/50",
+            {
+              "bg-gradient-to-b": vertical,
+              "bg-gradient-to-r": !vertical,
+            },
+          )}
         />
       )}
-    </>
+    </div>
   );
 }
