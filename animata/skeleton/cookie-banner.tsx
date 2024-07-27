3d80cb9e8103cf86c15c2f5399dc46975ca86838
Hari Lamichhane <onwithhari@gmail.com>
2024-07-27 14:05:26 +0545

    fix: make all skeleton same height

--- animata/skeleton/cookie-banner.tsx
+++ animata/skeleton/cookie-banner.tsx
@@ -1,6 +1,6 @@
 export default function CookieBanner() {
   return (
-    <div className="flex h-48 w-52 flex-col gap-3 rounded-md border bg-background p-3 shadow-xl transition-shadow hover:shadow-sm dark:border-zinc-700">
+    <div className="flex min-h-52 w-52 flex-col gap-3 rounded-md border bg-background p-3 shadow-xl transition-shadow hover:shadow-sm dark:border-zinc-700">
       <div className="flex flex-1 flex-col justify-center gap-2">
         <div className="h-2 w-1/2 rounded-md bg-muted-foreground/25" />
         <div className="h-2 w-3/4 rounded-md bg-muted" />
