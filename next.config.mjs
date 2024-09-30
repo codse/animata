e18eccfe9018e9cd43d606759d80a75bcb8888b6
Hari Lamichhane <onwithhari@gmail.com>
2024-09-30 09:55:44 +0545

    feat: update config

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ import { withContentlayer } from "next-contentlayer";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // Enable static exports
-  basePath: "/", // Replace with your repo name
   reactStrictMode: true,
   swcMinify: true,
 };
