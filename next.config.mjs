a64a5b0c9b4a6b5cf1790604d4f5c04648721d7a
Hari Lamichhane <onwithhari@gmail.com>
2024-09-30 09:06:12 +0545

    feat: add github action

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@ import { withContentlayer } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export", // Enable static exports
+  basePath: "/", // Replace with your repo name
   reactStrictMode: true,
   swcMinify: true,
 };
