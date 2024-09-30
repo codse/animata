6425ddba5f0790035c8169c5afb6d5ed130ed5d5
Sudha Shrestha <itsmesudha12@gmail.com>
2024-09-30 20:06:24 +0545

    feat: add hacktoberfest announcement

--- components/announcement.tsx
+++ components/announcement.tsx
@@ -1,19 +1,15 @@
-import Link from "next/link";
-import { ArrowRight, Combine } from "lucide-react";
+import { StarsIcon } from "lucide-react";
 
-import AnimatedGradientText from "@/animata/text/animated-gradient-text";
-import { Separator } from "@/components/ui/separator";
-
-export function Announcement() {
+export function Announcement({ text }: { text: string }) {
   return (
-    <div className="group z-10 mx-auto mb-4 w-fit rounded-full bg-background px-2 lg:-mb-20">
-      <Link href="/docs/changelog">
-        <AnimatedGradientText className="inline-flex cursor-pointer items-center rounded-full border border-sky-500 from-rose-500 via-green-600 to-purple-500 px-4 py-2 text-sm font-semibold hover:border-sky-600">
-          <Combine className="h-4 w-4 text-blue-500" />{" "}
-          <Separator className="mx-3 h-4" orientation="vertical" /> <span>Animata is live</span>
-          <ArrowRight className="ml-1 h-4 w-4 text-blue-500 transition-all group-hover:translate-x-2" />{" "}
-        </AnimatedGradientText>
-      </Link>
+    <div className="group relative isolate z-50 mx-auto w-fit cursor-pointer p-4">
+      <div className="rounded-lg bg-gradient-to-r from-purple-100 to-yellow-100 p-1 shadow-lg transition-all duration-300 ease-in-out group-hover:scale-105 group-hover:shadow-xl">
+        <div className="flex items-center justify-between px-4 py-2">
+          <StarsIcon className="h-4 w-4 text-purple-500" />
+          <span className="mx-2 text-sm font-medium text-gray-800">{text}</span>
+          <StarsIcon className="h-4 w-4 text-yellow-500" />
+        </div>
+      </div>
     </div>
   );
 }
