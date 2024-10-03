6acb580d8671ecef2bc30b073fdc46d060f33978
Prithwi <prithwi.online11@gmail.com>
2024-10-03 14:26:40 +0530

    feat(card): remove card skew on btn hover in feature card

--- animata/card/feature-card.tsx
+++ animata/card/feature-card.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { Check } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
@@ -15,19 +14,14 @@ export default function FeatureCard({
   placeholder = "justin@buttondown.email",
   buttonText = "Subscribe for $5/mo",
 }: FeatureCardProps) {
-  const [isButtonHovered, setIsButtonHovered] = useState(false);
-
   return (
-    <div
-      className={`mx-auto flex w-full max-w-lg ${isButtonHovered ? "skew-x-0 skew-y-0" : "-skew-x-3 skew-y-2"} flex-col gap-2 rounded-lg bg-white py-6 transition-transform duration-300`}
-    >
+    <div className="mx-auto flex w-full max-w-lg -skew-x-3 skew-y-2 flex-col gap-2 rounded-lg bg-white py-6 transition-transform duration-300 hover:skew-x-0 hover:skew-y-0">
       <h3 className="px-8 font-sans font-bold text-black">{title}</h3>
       <div className="flex flex-col gap-2 px-4">
         <Input placeholder={placeholder} className="rounded-none bg-gray-100" />
         <Button
-          className="relative cursor-pointer bg-gray-500 text-white transition-all duration-300 hover:bg-green-600"
-          onMouseEnter={() => setIsButtonHovered(true)}
-          onMouseLeave={() => setIsButtonHovered(false)}
+          className="group relative cursor-pointer bg-gray-500 text-white transition-all duration-300 hover:bg-green-600"
+          aria-label={buttonText}
         >
           <Check className="absolute left-3 opacity-0 transition-opacity duration-300 group-hover:opacity-100" />
           <p className="pl-4 hover:font-bold">{buttonText}</p>
