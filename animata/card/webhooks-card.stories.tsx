64cc782b302f7ba6d6faf0a49370687f4b4f806a
Pavankumar07s <pawankumar14662693@gmail.com>
2024-10-05 22:41:17 +0530

    feat(card): add Webhooks-card 008

---
+++ animata/card/webhooks-card.stories.tsx
@@ -0,0 +1,22 @@
+import { WebHooks } from "@/animata/card/WebHooks-card";
+import { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+  title: "Card/Web Hooks",
+  component: WebHooks,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {},
+} satisfies Meta<typeof WebHooks>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    leftBoxElem: "Button Down ",
+    rightBoxElem: "Your app",
+  },
+};
