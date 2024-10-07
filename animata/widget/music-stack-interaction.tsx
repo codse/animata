24120efe0dbc8bc9a0cde72e24c561a8c2bc6351
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2024-10-07 05:26:38 +0000

    refactor: update transition duration in music stack interaction

--- animata/widget/music-stack-interaction.tsx
+++ animata/widget/music-stack-interaction.tsx
@@ -12,7 +12,7 @@ const carouselStyles = {
 const carouselInnerStyles: React.CSSProperties = {
   display: "flex",
   transformStyle: "preserve-3d",
-  transition: "transform 1s",
+  transition: "transform 2s",
   justifyContent: "center",
   alignItems: "center",
   height: "100%",
@@ -24,7 +24,7 @@ const carouselItemStyles: React.CSSProperties = {
   transform: "rotateY(15deg) translateZ(300px) translateX(-50px)",
   backfaceVisibility: "hidden",
   boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
-  transition: "transform 0.5s, box-shadow 0.5s",
+  transition: "transform 2s, box-shadow 2s",
 };
 
 const carouselItemFirstChildStyles: React.CSSProperties = {
@@ -33,19 +33,19 @@ const carouselItemFirstChildStyles: React.CSSProperties = {
   transform: "rotateY(5deg) translateZ(300px) translateX(0)",
   backfaceVisibility: "hidden",
   boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
-  transition: "transform 0.5s, box-shadow 0.5s",
+  transition: "transform 2s, box-shadow 2s",
 };
 
-const carouselBackgroundStyles: React.CSSProperties = {
-  position: "absolute",
-  top: 0,
-  left: 0,
-  width: "100%",
-  height: "100%",
-  backgroundSize: "cover",
-  backgroundPosition: "center",
-  opacity: 0.8,
-};
+// const carouselBackgroundStyles: React.CSSProperties = {
+//   position: "absolute",
+//   top: 0,
+//   left: 0,
+//   width: "100%",
+//   height: "100%",
+//   backgroundSize: "cover",
+//   backgroundPosition: "center",
+//   opacity: 0.6,
+// };
 
 interface albumsProps {
   /*
@@ -63,7 +63,7 @@ interface albumsProps {
   albumCover: string;
 }
 
-export default function MusicStackInteraction({ albums, albumCover }: albumsProps) {
+export default function MusicStackInteraction({ albums }: albumsProps) {
   const [isGridView, setIsGridView] = useState(true);
 
   const handleToggleView = () => {
@@ -72,54 +72,56 @@ export default function MusicStackInteraction({ albums, albumCover }: albumsProp
 
   return (
     <div className="relative mx-auto h-[33rem] w-80 rounded bg-gray-900 p-2 text-white">
+      {/* <motion.div
+        // style={
+        //   { ...carouselBackgroundStyles, backgroundImage: `url(${albumCover})` , zIndex: g}
+        // }
+        // layout
+      > */}
       <motion.div
-        style={
-          isGridView
-            ? undefined
-            : { ...carouselBackgroundStyles, backgroundImage: `url(${albumCover})` }
-        }
+        className={cn("h-96 w-full", { "mt-24": !isGridView })}
+        style={isGridView ? undefined : carouselStyles}
         layout
       >
         <motion.div
-          className={cn("h-96 w-full", { "mt-24": !isGridView })}
-          style={isGridView ? undefined : carouselStyles}
+          className={cn("", { "mb-4 grid grid-cols-2 gap-6": isGridView })}
+          style={isGridView ? undefined : carouselInnerStyles}
           layout
         >
-          <motion.div
-            className={cn("", { "mb-4 grid grid-cols-2 gap-6": isGridView })}
-            style={isGridView ? undefined : carouselInnerStyles}
-            layout
-          >
-            {albums.map((album, index) => (
-              <div
-                key={album.id}
-                className={cn("relative w-full shadow-lg")}
-                style={
-                  isGridView
-                    ? undefined
-                    : index === 0
-                      ? carouselItemFirstChildStyles
-                      : carouselItemStyles
-                }
+          {albums.map((album, index) => (
+            <div
+              key={album.id}
+              className={cn("relative w-full shadow-lg")}
+              style={
+                isGridView
+                  ? undefined
+                  : index === 0
+                    ? carouselItemFirstChildStyles
+                    : carouselItemStyles
+              }
+            >
+              <motion.img
+                layout
+                src={album.cover + "?w=200&h=200"}
+                alt={album.title}
+                className="h-auto rounded-xl shadow-md"
+              />
+              <motion.div
+                layout
+                className="absolute bottom-0 left-0 w-full bg-opacity-50 bg-gradient-to-b from-transparent to-gray-800 px-4 py-2 text-white"
               >
-                <motion.img
-                  layout
-                  src={album.cover + "?w=200&h=200"}
-                  alt={album.title}
-                  className="h-auto rounded-xl shadow-md"
-                />
-                <motion.div
-                  layout
-                  className="absolute bottom-0 left-0 w-full bg-opacity-50 bg-gradient-to-b from-transparent to-gray-800 px-4 py-2 text-white"
-                >
-                  <h3 className="font-semibold leading-tight">{album.title}</h3>
-                  <p className="text-sm leading-snug">{album.artist}</p>
-                </motion.div>
-              </div>
-            ))}
-          </motion.div>
+                <motion.h3 layout className="font-semibold leading-tight">
+                  {album.title}
+                </motion.h3>
+                <motion.p layout className="text-sm leading-snug">
+                  {album.artist}
+                </motion.p>
+              </motion.div>
+            </div>
+          ))}
         </motion.div>
       </motion.div>
+      {/* </motion.div> */}
 
       <motion.div className="fixed bottom-4 left-0 right-0 mx-8 mb-4 flex w-auto items-center justify-center rounded-xl bg-gray-800 p-4 text-white shadow-2xl transition-all duration-1000">
         <div className="flex w-32 items-center space-x-2 rounded-full bg-gray-900 p-2">
