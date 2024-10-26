3da73cf6e73e2cefc6d8515140a74924702c1bec
Christine <shecoder30@gmail.com>
2024-10-26 22:09:04 +0000

    feat: add style to the button
    Signed-off-by: Christine <shecoder30@gmail.com>

--- animata/button/form-control-switch.tsx
+++ animata/button/form-control-switch.tsx
@@ -1,9 +1,24 @@
+import React, { useState } from "react";
 export default function FormControlSwitch() {
+  const [isOn, setIsOn] = useState(false);
+  const toggleSwitch = () => {
+    setIsOn(!isOn);
+  };
   return (
-    <div>
-      <p>
-        Let your <i>imagination</i> run wild with this template
-      </p>
-    </div>
+    <button onClick={toggleSwitch} style={{ backgroundColor: isOn ? "green" : "red" }}>
+      <div style={{ ...styles.switch, backgroundColor: isOn ? "green" : "grey" }}>
+        {isOn ? "✔️" : "❌"}
+      </div>
+    </button>
   );
 }
+const styles = {
+  switch: {
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+};
