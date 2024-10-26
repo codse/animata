eb4a31b25374a63678b3051bb68b7cc2faa25194
Christine <shecoder30@gmail.com>
2024-10-26 19:52:58 +0000

    feat: add form-control-switch button
    Signed-off-by: Christine <shecoder30@gmail.com>

---
+++ animata/button/form-control-switch.stories.tsx
@@ -0,0 +1,19 @@
+import FormControlSwitch from "@/animata/button/form-control-switch";
+import { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+  title: "Button/Form Control Switch",
+  component: FormControlSwitch,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {},
+} satisfies Meta<typeof FormControlSwitch>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {},
+};
---
+++ animata/button/form-control-switch.tsx
@@ -0,0 +1,9 @@
+export default function FormControlSwitch() {
+  return (
+    <div>
+      <p>
+        Let your <i>imagination</i> run wild with this template
+      </p>
+    </div>
+  );
+}
